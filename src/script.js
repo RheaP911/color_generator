c3383a364cd3ff29cc75e00bfb27f7281ad4f62b
RheaP911 <rhealynpanganiban20@gmail.com>
2025-03-04 15:03:22 +0800

    added copy function

--- src/script.js
+++ src/script.js
@@ -1,15 +1,4 @@
-// function copyCode(text) {
-//     var copyText = document.getElementsByClassName('hexcode');
-
-//     // select text field
-//     copyText.select();
-//     copyText.setSelectionRange(0, 99999);
-
-//     navigator.clipboard.writeText(copyText.innerText);
-//     alert("Hex code copied!")
-// }
-
-function generatePallete() {
+function generateColors() {
     const hex1 = document.getElementById('hexcode1');
     const hex2 = document.getElementById('hexcode2');
     const hex3 = document.getElementById('hexcode3');
@@ -22,49 +11,30 @@ function generatePallete() {
     const card5 = document.getElementById('card5');
 
     const letters = ['A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'];
-    
     let length = letters.length;
 
-    let randomCode1 = "";
-    for (let i = 0; i < 6; i++) {
-        let randomIndex = Math.floor(Math.random() * length);
-        randomCode1 += letters[randomIndex];
-        hexCode1 = "#" + randomCode1
+    function generateHex() {
+        let hex = "#";
+        for (let i = 0; i < 6; i++) {
+            let randomIndex = Math.floor(Math.random() * length);
+            hex += letters[randomIndex];
+        }
+        return hex;
     }
 
-    let randomCode2 = "";
-    for (let i = 0; i < 6; i++) {
-        let randomIndex = Math.floor(Math.random() * length);
-        randomCode2 += letters[randomIndex];
-        hexCode2 = "#" + randomCode2
-    }
+    let hexCode1 = generateHex();
+    let hexCode2 = generateHex();
+    let hexCode3 = generateHex();
+    let hexCode4 = generateHex();
+    let hexCode5 = generateHex();
 
-    let randomCode3 = "";
-    for (let i = 0; i < 6; i++) {
-        let randomIndex = Math.floor(Math.random() * length);
-        randomCode3 += letters[randomIndex];
-        hexCode3 = "#" + randomCode3
-    }
 
-    let randomCode4 = "";
-    for (let i = 0; i < 6; i++) {
-        let randomIndex = Math.floor(Math.random() * length);
-        randomCode4 += letters[randomIndex];
-        hexCode4 = "#" + randomCode4
-    }
 
-    let randomCode5 = "";
-    for (let i = 0; i < 6; i++) {
-        let randomIndex = Math.floor(Math.random() * length);
-        randomCode5 += letters[randomIndex];
-        hexCode5 = "#" + randomCode5
-    }
-
-    hex1.innerHTML = hexCode1;
-    hex2.innerHTML = hexCode2;
-    hex3.innerHTML = hexCode3;
-    hex4.innerHTML = hexCode4;
-    hex5.innerHTML = hexCode5;
+    hex1.innerText = hexCode1;
+    hex2.innerText = hexCode2;
+    hex3.innerText = hexCode3;
+    hex4.innerText = hexCode4;
+    hex5.innerText = hexCode5;
 
     card1.style.backgroundColor = hexCode1;
     card2.style.backgroundColor = hexCode2;
@@ -72,4 +42,20 @@ function generatePallete() {
     card4.style.backgroundColor = hexCode4;
     card5.style.backgroundColor = hexCode5;
 
+    // Add click event to copy hex code
+    function addCopyListener(card, hexCode) {
+        card.addEventListener("click", () => {
+            navigator.clipboard.writeText(hexCode).then(() => {
+                alert(`Hex code ${hexCode} copied to clipboard!`);
+            }).catch(err => {
+                console.error("Failed to copy text", err);
+            });
+        });
+    }
+
+    addCopyListener(card1, hexCode1);
+    addCopyListener(card2, hexCode2);
+    addCopyListener(card3, hexCode3);
+    addCopyListener(card4, hexCode4);
+    addCopyListener(card5, hexCode5);
 }
